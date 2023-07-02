38d97430d3f37baa30eea21e3754b6b0f223320e
Smithabyss <smith.abyss@gmail.com>
2023-07-02 12:07:46 -0300

    implemented js random number to the dices, but there's a bug i cannot solve: every time I click the second dice it changes the first one

--- JogarDados.js
+++ JogarDados.js
@@ -1,4 +1,31 @@
 function JogarDados()
 {
-    document.getElementById("texto").innerHTML = "Mudei tudo";
+    var img = document.querySelector('#img');
+    var num = Math.floor(1+Math.random()*6); 
+
+    if(num === 1)
+    {
+        img.src = 'Assets/Dado1.png';
+    }
+    if(num === 2)
+    {
+        img.src = 'Assets/Dado2.png';
+    }
+    if(num === 3)
+    {
+        img.src = 'Assets/Dado3.png';
+    }
+    if(num === 4)
+    {
+        img.src = 'Assets/Dado4.png';
+    }
+    if(num === 5)
+    {
+        img.src = 'Assets/Dado5.png';
+    }
+
+    if(num === 6)
+    {
+        img.src = 'Assets/Dado6.png';
+    }
 }
