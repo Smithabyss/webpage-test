a5ba6bacfe856ef81fd9a34b9817201a55b906a6
Smithabyss <smith.abyss@gmail.com>
2023-07-02 21:58:25 -0300

    changed css to scss. Completed the js

--- JogarDados.js
+++ JogarDados.js
@@ -1,8 +1,24 @@
 function JogarDados()
 {
-    var img = document.querySelectorAll('img')[0];
-    var num = Math.floor(1+Math.random()*6); 
-    var srcImg = "Assets/Dado"+ num +".png";
-    img.setAttribute('src', srcImg);
+    var img1 = document.querySelectorAll('img')[0];
+    var num1 = Math.floor(1+Math.random()*6); 
+    var srcImg = "Assets/Dado"+ num1 +".png";
+    img1.setAttribute('src', srcImg);
 
+    var img2 = document.querySelectorAll('img')[1];
+    var num2 = Math.floor(1+Math.random()*6); 
+    var srcImg = "Assets/Dado"+ num2 +".png";
+    img2.setAttribute('src', srcImg);
+
+
+    if(num1 > num2)
+    {
+        document.getElementById('texto').innerHTML = "Jogador 1 ganhou!"
+    }
+    else if(num1 === num2)
+    {
+        document.getElementById('texto').innerHTML = "Empate!"
+    }
+    else
+        document.getElementById('texto').innerHTML = "Jogador 2 ganhou!"
 }
